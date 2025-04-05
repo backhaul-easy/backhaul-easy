f87e7a065c78329042a2dfdb71e1c77075695a2b
Masih Jahangiri <masihjahan@gmail.com>
2025-04-05 07:48:20 +0330

    chore: remove CHANGELOG.md and update commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,21 +1,3 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
-        'build',
-        'ci',
-        'chore'
-      ]
-    ]
-  }
-} 
+  extends: ['@commitlint/config-conventional']
+};
